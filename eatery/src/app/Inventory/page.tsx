d11c48b623d37e1de13ad62681271d990d92b2e4
Toushik-Ahmed <pronay5271@gmail.com>
2024-09-09 18:18:14 +0600

    inventoryStarted

---
+++ eatery/src/app/Inventory/page.tsx
@@ -0,0 +1,14 @@
+import SideNavbar from '@/components/inventoryComponent/SideNavbar'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <SideNavbar/>
+    </div>
+  )
+}
+
+export default page
