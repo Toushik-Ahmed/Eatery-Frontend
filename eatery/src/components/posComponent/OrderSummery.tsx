bf28b2f502ae6647cdb3d3af008b2a94f5979506
Rafsan2507 <khanrafsan25@gmail.com>
2024-09-10 18:57:10 +0600

    order summery

--- eatery/src/components/posComponent/OrderSummery.tsx
+++ eatery/src/components/posComponent/OrderSummery.tsx
@@ -5,7 +5,12 @@ type Props = {}
 const OrderSummery = (props: Props) => {
   return (
     <div className='w-[25vw] h-[85vh] border border-black'>
+        <div className='mx-[1vw]'>
         <h2>Order Summery</h2>
+        <div className='h-[10vh] mt-[2vh] border border-black rounded-[1vh]'>
+
+        </div>
+        </div>
     </div>
   )
 }
