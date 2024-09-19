dd358d5bd77af0c1c62783d7f655912a4fb31376
Rizwan <rizwanfahim24@gmail.com>
2024-09-19 18:36:13 +0600

    basic

---
+++ eatery/src/app/table/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import PersistentDrawerLeft from "@/components/tableComponents/SideNavBar";
+import React from "react";
+
+type Props = {};
+
+function Page({}: Props) {
+  return (
+    <div>
+      <PersistentDrawerLeft />
+    </div>
+  );
+}
+
+export default Page;
