f24f8d0c4d91b80f9cd379349fe7d8cc6d0acbc7
Rizwan <rizwanfahim24@gmail.com>
2024-09-21 04:10:53 +0600

    Table Done

--- eatery/src/components/tableComponents/SideNavBar.tsx
+++ eatery/src/components/tableComponents/SideNavBar.tsx
@@ -1,3 +1,243 @@
+// "use client";
+// import * as React from "react";
+// import { styled, useTheme } from "@mui/material/styles";
+// import Box from "@mui/material/Box";
+// import Drawer from "@mui/material/Drawer";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+// import Toolbar from "@mui/material/Toolbar";
+// import List from "@mui/material/List";
+// import Typography from "@mui/material/Typography";
+// import Divider from "@mui/material/Divider";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+// import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import Button from "@mui/material/Button";
+// import DeleteIcon from "@mui/icons-material/Delete";
+
+// import DashboardIcon from "@mui/icons-material/Dashboard";
+// import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+// import FolderSharedIcon from "@mui/icons-material/FolderShared";
+// import InventoryIcon from "@mui/icons-material/Inventory";
+// import AddCircleIcon from "@mui/icons-material/AddCircle";
+// import WidgetsIcon from "@mui/icons-material/Widgets";
+
+// const drawerWidth = 210;
+
+// const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+//   open?: boolean;
+// }>(({ theme }) => ({
+//   flexGrow: 1,
+//   padding: theme.spacing(3),
+//   transition: theme.transitions.create("margin", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   marginLeft: `-${drawerWidth}px`,
+//   variants: [
+//     {
+//       props: ({ open }) => open,
+//       style: {
+//         transition: theme.transitions.create("margin", {
+//           easing: theme.transitions.easing.easeOut,
+//           duration: theme.transitions.duration.enteringScreen,
+//         }),
+//         marginLeft: 0,
+//       },
+//     },
+//   ],
+// }));
+
+// interface AppBarProps extends MuiAppBarProps {
+//   open?: boolean;
+// }
+
+// const AppBar = styled(MuiAppBar, {
+//   shouldForwardProp: (prop) => prop !== "open",
+// })<AppBarProps>(({ theme }) => ({
+//   transition: theme.transitions.create(["margin", "width"], {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   backgroundColor: "#e91e63",
+//   color: "black",
+//   variants: [
+//     {
+//       props: ({ open }) => open,
+//       style: {
+//         width: `calc(100% - ${drawerWidth}px)`,
+//         marginLeft: `${drawerWidth}px`,
+//         transition: theme.transitions.create(["margin", "width"], {
+//           easing: theme.transitions.easing.easeOut,
+//           duration: theme.transitions.duration.enteringScreen,
+//         }),
+//       },
+//     },
+//   ],
+// }));
+
+// const DrawerHeader = styled("div")(({ theme }) => ({
+//   display: "flex",
+//   alignItems: "center",
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+//   justifyContent: "flex-end",
+// }));
+
+// export default function PersistentDrawerLeft() {
+//   const theme = useTheme();
+//   const [open, setOpen] = React.useState(true);
+
+//   const handleDrawerOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleDrawerClose = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       <CssBaseline />
+//       <AppBar position="fixed" open={open}>
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             onClick={handleDrawerOpen}
+//             edge="start"
+//             sx={[
+//               {
+//                 mr: 2,
+//               },
+//               open && { display: "none" },
+//             ]}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           {/* <Typography variant="h6" noWrap component="div">
+//             Eatery
+//           </Typography> */}
+//         </Toolbar>
+//       </AppBar>
+//       <Drawer
+//         sx={{
+//           width: drawerWidth,
+//           flexShrink: 0,
+//           "& .MuiDrawer-paper": {
+//             width: drawerWidth,
+//             boxSizing: "border-box",
+//           },
+//         }}
+//         variant="persistent"
+//         anchor="left"
+//         open={open}
+//       >
+//         <DrawerHeader>
+//           Eatery
+//           <IconButton onClick={handleDrawerClose}>
+//             {theme.direction === "ltr" ? (
+//               <ChevronLeftIcon />
+//             ) : (
+//               <ChevronRightIcon />
+//             )}
+//           </IconButton>
+//         </DrawerHeader>
+//         <Divider />
+//         <List>
+//           {[
+//             "Dashboard",
+//             "POS",
+//             "HR Directory",
+//             "Inventory",
+//             "Add Menu",
+//             "Menu",
+//           ].map((text, index) => (
+//             <ListItem key={text} disablePadding>
+//               <ListItemButton
+//                 sx={{
+//                   "&:hover": {
+//                     backgroundColor: "#00bcd4",
+//                     color: "white",
+//                   },
+//                 }}
+//               >
+//                 <ListItemIcon
+//                   sx={{
+//                     minWidth: "40px",
+//                   }}
+//                 >
+//                   {index === 0 && <DashboardIcon />}
+//                   {index === 1 && <PointOfSaleIcon />}
+//                   {index === 2 && <FolderSharedIcon />}
+//                   {index === 3 && <InventoryIcon />}
+//                   {index === 4 && <AddCircleIcon />}
+//                   {index === 5 && <WidgetsIcon />}
+//                 </ListItemIcon>
+//                 <ListItemText primary={text} />
+//               </ListItemButton>
+//             </ListItem>
+//           ))}
+//         </List>
+//         <Divider />
+//         {/* <List>
+//           {["All mail", "Trash", "Spam"].map((text, index) => (
+//             <ListItem key={text} disablePadding>
+//               <ListItemButton>
+//                 <ListItemIcon>
+//                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//                 </ListItemIcon>
+//                 <ListItemText primary={text} />
+//               </ListItemButton>
+//             </ListItem>
+//           ))}
+//         </List> */}
+//       </Drawer>
+//       <Main open={open}>
+//         <DrawerHeader />
+//         <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+//           <Button variant="outlined">Add Table</Button>
+//         </Box>
+//         {/* <Typography sx={{ marginBottom: 2 }}>
+//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+//           dolor purus non enim praesent elementum facilisis leo vel. Risus at
+//           ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+//           quisque non tellus. Convallis convallis tellus id interdum velit
+//           laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+//           adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+//           integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+//           eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+//           quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+//           vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+//           lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+//           faucibus et molestie ac.
+//         </Typography>
+//         <Typography sx={{ marginBottom: 2 }}>
+//           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+//           ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+//           elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+//           sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+//           mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+//           risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+//           purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+//           tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+//           morbi tristique senectus et. Adipiscing elit duis tristique
+//           sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+//           eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+//           posuere sollicitudin aliquam ultrices sagittis orci a.
+//         </Typography> */}
+//       </Main>
+//     </Box>
+//   );
+// }
+
 "use client";
 import * as React from "react";
 import { styled, useTheme } from "@mui/material/styles";
@@ -17,7 +257,8 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import FolderSharedIcon from "@mui/icons-material/FolderShared";
@@ -91,6 +332,9 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 export default function PersistentDrawerLeft() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
+  const [rightDrawerOpen, setRightDrawerOpen] = React.useState(false); // State for the right drawer
+  const [tableNumber, setTableNumber] = React.useState("");
+  const [seatingCapacity, setSeatingCapacity] = React.useState("");
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -98,6 +342,21 @@ export default function PersistentDrawerLeft() {
 
   const handleDrawerClose = () => {
     setOpen(false);
+  };
+
+  const handleRightDrawerOpen = () => {
+    setRightDrawerOpen(true);
+  };
+
+  const handleRightDrawerClose = () => {
+    setRightDrawerOpen(false);
+  };
+
+  const handleAddTable = () => {
+    console.log("Table Number:", tableNumber);
+    console.log("Seating Capacity:", seatingCapacity);
+    // Perform actions like API call here to add the table
+    handleRightDrawerClose(); // Close drawer after adding table
   };
 
   return (
@@ -119,9 +378,6 @@ export default function PersistentDrawerLeft() {
           >
             <MenuIcon />
           </IconButton>
-          {/* <Typography variant="h6" noWrap component="div">
-            Eatery
-          </Typography> */}
         </Toolbar>
       </AppBar>
       <Drawer
@@ -184,51 +440,59 @@ export default function PersistentDrawerLeft() {
           ))}
         </List>
         <Divider />
-        {/* <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button variant="outlined" onClick={handleRightDrawerOpen}>
+            Add Table
+          </Button>
+        </Box>
       </Main>
+      {/* Right-side drawer for adding tables */}
+      <Drawer
+        anchor="right"
+        open={rightDrawerOpen}
+        onClose={handleRightDrawerClose}
+        sx={{
+          "& .MuiDrawer-paper": {
+            width: 300,
+            padding: 2,
+          },
+        }}
+      >
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <IconButton onClick={handleRightDrawerClose}>
+            <ChevronRightIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ padding: 2 }}>
+          <Typography variant="h6" gutterBottom>
+            Add Table
+          </Typography>
+          <TextField
+            label="Table Number"
+            value={tableNumber}
+            onChange={(e) => setTableNumber(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Seating Capacity"
+            value={seatingCapacity}
+            onChange={(e) => setSeatingCapacity(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <Box
+            sx={{ display: "flex", justifyContent: "flex-end", marginTop: 2 }}
+          >
+            <Button variant="contained" onClick={handleAddTable}>
+              Add Table
+            </Button>
+          </Box>
+        </Box>
+      </Drawer>
     </Box>
   );
 }
---
+++ eatery/src/components/tableTryComponents/SideNavTry.tsx
@@ -0,0 +1,318 @@
+import * as React from "react";
+import Link from "next/link";
+import { styled, useTheme, Theme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import FolderSharedIcon from "@mui/icons-material/FolderShared";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import WidgetsIcon from "@mui/icons-material/Widgets";
+import {
+  Table1,
+  Table2,
+  Table3,
+  Table4,
+  Table5,
+  Table6,
+  Table7,
+  Table8,
+} from "./tableTry";
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+interface TableData {
+  number: number;
+  capacity: number;
+}
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+  const [rightDrawerOpen, setRightDrawerOpen] = React.useState(false);
+  const [tableNumber, setTableNumber] = React.useState("");
+  const [seatingCapacity, setSeatingCapacity] = React.useState("");
+  const [tables, setTables] = React.useState<TableData[]>([]);
+
+  React.useEffect(() => {
+    const storedTables = localStorage.getItem("tables");
+    if (storedTables) {
+      setTables(JSON.parse(storedTables));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (tables.length > 0) {
+      localStorage.setItem("tables", JSON.stringify(tables));
+    }
+  }, [tables]);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleRightDrawerClose = () => {
+    setRightDrawerOpen(false);
+  };
+
+  const handleRightDrawerOpen = () => {
+    setRightDrawerOpen(true);
+  };
+
+  const handleAddTable = () => {
+    if (tableNumber && seatingCapacity) {
+      const newTable: TableData = {
+        number: parseInt(tableNumber),
+        capacity: parseInt(seatingCapacity),
+      };
+      const updatedTables = [...tables, newTable];
+      setTables(updatedTables);
+      localStorage.setItem("tables", JSON.stringify(updatedTables));
+      setTableNumber("");
+      setSeatingCapacity("");
+      handleRightDrawerClose();
+    }
+  };
+
+  const handleDeleteTable = (tableNumber: number) => {
+    const updatedTables = tables.filter(
+      (table) => table.number !== tableNumber
+    );
+    setTables(updatedTables);
+    localStorage.setItem("tables", JSON.stringify(updatedTables));
+  };
+
+  const renderTable = (table: TableData) => {
+    const TableComponent =
+      {
+        1: Table1,
+        2: Table2,
+        3: Table3,
+        4: Table4,
+        5: Table5,
+        6: Table6,
+        7: Table7,
+        8: Table8,
+      }[table.capacity] || Table4;
+
+    return (
+      <Box key={table.number} onClick={(e) => e.preventDefault()}>
+        <Link href={`/order?table=${table.number}`} passHref>
+          <TableComponent
+            tableNumber={table.number}
+            onDelete={(e: React.MouseEvent) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleDeleteTable(table.number);
+            }}
+          />
+        </Link>
+      </Box>
+    );
+  };
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Table Management
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {[
+            "Dashboard",
+            "POS",
+            "HR Directory",
+            "Inventory",
+            "Add Menu",
+            "Menu",
+          ].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index === 0 && <DashboardIcon />}
+                  {index === 1 && <PointOfSaleIcon />}
+                  {index === 2 && <FolderSharedIcon />}
+                  {index === 3 && <InventoryIcon />}
+                  {index === 4 && <AddCircleIcon />}
+                  {index === 5 && <WidgetsIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button variant="outlined" onClick={handleRightDrawerOpen}>
+            Add Table
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            marginTop: 2,
+          }}
+        >
+          {tables.map(renderTable)}
+        </Box>
+      </Main>
+      <Drawer
+        anchor="right"
+        open={rightDrawerOpen}
+        onClose={handleRightDrawerClose}
+        sx={{
+          "& .MuiDrawer-paper": {
+            width: 300,
+            padding: 2,
+          },
+        }}
+      >
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <IconButton onClick={handleRightDrawerClose}>
+            <ChevronRightIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ padding: 2 }}>
+          <Typography variant="h6" gutterBottom>
+            Add Table
+          </Typography>
+          <TextField
+            label="Table Number"
+            value={tableNumber}
+            onChange={(e) => setTableNumber(e.target.value)}
+            fullWidth
+            margin="normal"
+            type="number"
+          />
+          <TextField
+            label="Seating Capacity"
+            value={seatingCapacity}
+            onChange={(e) => setSeatingCapacity(e.target.value)}
+            fullWidth
+            margin="normal"
+            type="number"
+          />
+          <Box
+            sx={{ display: "flex", justifyContent: "flex-end", marginTop: 2 }}
+          >
+            <Button
+              variant="contained"
+              onClick={handleAddTable}
+              disabled={!tableNumber || !seatingCapacity}
+            >
+              Add Table
+            </Button>
+          </Box>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+}
