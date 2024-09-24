535bad1795e7894307d6e9be2664c50a350039c2
Toushik-Ahmed <pronay5271@gmail.com>
2024-09-24 18:18:07 +0600

    d

--- eatery/src/components/inventoryComponent/OrderComponent.tsx
+++ eatery/src/components/inventoryComponent/OrderComponent.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 import VendorCard from './VendorCard';
 import vendor from './assets/meatVendor.avif';
 import vegetable from './assets/vegetables-and-fruits-farmers-market.webp';
@@ -9,33 +8,11 @@ type Props = {
 };
 
 const OrderComponent = ({ handleClick }: Props) => {
-  // const [ingredient, setIngredient] = useState('');
-  // const [quantity, setQuantity] = useState('');
-  // const [unit, setUnit] = useState('');
-  // const [alergens, setAlergens] = useState<string>('No');
-
-  // const handleSubmit = () => {
-  //   const orderForm = {
-  //     ingredient,
-  //     quantity,
-  //     unit,
-
-  //   };
-  //   console.log(orderForm); // for testing
-  // };
-
-  // const handleCancel = () => {
-  //   setAlergens('');
-  //   setIngredient('');
-  //   setQuantity('');
-  //   setUnit('');
-  // };
-
   return (
     <div className="m-4">
       <div className="font-bold text-3xl mb-10">Vendor-Lists</div>
 
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-x- gap-y-4" >
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-x- gap-y-4">
         <VendorCard
           src={vendor.src}
           alt="vendorPic"
