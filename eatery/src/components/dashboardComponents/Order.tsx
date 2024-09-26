aecc1900646f0dbee73c3a09bc33c3bf4ddb3b89
Rizwan <rizwanfahim24@gmail.com>
2024-09-26 15:04:04 +0600

    adios

--- eatery/src/components/dashboardComponents/Order.tsx
+++ eatery/src/components/dashboardComponents/Order.tsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import orderToday from "@/services/dashboardServices/orderToday";
-
-// type Props = {};
-
-// function Order({}: Props) {
-//   const [order, setOrder] = useState("");
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await orderToday();
-//         console.log("response from Order Component: ", response);
-//         setOrder(response);
-//       } catch (error) {
-//         console.error("Error fetching data: ", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Today's Total Order</h1>
-//       <h1>{order ? JSON.stringify(order) : "Loading..."}</h1>
-//     </div>
-//   );
-// }
-
-// export default Order;
-
 import React, { useEffect, useState } from "react";
 import {
   Card,
