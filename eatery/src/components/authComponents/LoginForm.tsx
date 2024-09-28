4c331f355f56929fcb98e0feb9022f666f8717cb
Rizwan <rizwanfahim24@gmail.com>
2024-09-28 16:16:56 +0600

    final

--- eatery/src/components/authComponents/LoginForm.tsx
+++ eatery/src/components/authComponents/LoginForm.tsx
@@ -70,7 +70,7 @@ const LoginForm = ({}: Props) => {
         router.push("/dashboard");
       }
       if (userType === "POSManager") {
-        router.push("/Inventory");
+        router.push("/tableTry");
       }
       if (userType === "MenuManager") {
         router.push("/menubuilder");
