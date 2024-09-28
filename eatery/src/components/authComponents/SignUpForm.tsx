5f832c0e359f016060e92df5552e1d4eeeab28fa
hossainsmshakib <smsakib2403@gmail.com>
2024-09-28 12:34:32 +0600

    login signup updated

--- eatery/src/components/authComponents/SignUpForm.tsx
+++ eatery/src/components/authComponents/SignUpForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import {  signUp } from "@/services/apiservice";
+import { signUp } from "@/services/apiservice";
 import {
   Box,
   Button,
@@ -16,11 +16,12 @@ import {
   Link,
   Select,
   Text,
-  VStack,
   Stack,
   useBreakpointValue,
+  useToast,
 } from "@chakra-ui/react";
 import { setToken } from "@/services/tokenServices";
+import Image from "next/image";
 
 type Props = {};
 
@@ -32,13 +33,14 @@ function SignUpForm({}: Props) {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
   const [signupSuccess, setSignupSuccess] = useState(false);
-  const [isHydrated, setIsHydrated] = useState(false); // Track hydration status
+  const [isHydrated, setIsHydrated] = useState(false);
 
+  const toast = useToast();
   const router = useRouter();
 
-  // Ensure component is hydrated
   useEffect(() => {
     setIsHydrated(true);
   }, []);
@@ -47,6 +49,18 @@ function SignUpForm({}: Props) {
     event.preventDefault();
     setError("");
     setSignupSuccess(false);
+
+    if (password !== confirmPassword) {
+      toast({
+        title: "Passwords do not match",
+        description: "Please ensure both password fields are identical.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      return;
+    }
 
     try {
       const accessTokenResponse = await signUp({
@@ -60,7 +74,6 @@ function SignUpForm({}: Props) {
       });
       setToken(accessTokenResponse.token);
       setSignupSuccess(true);
-    
       router.push("/login");
     } catch (error) {
       console.error("Error signing up:", error);
@@ -68,205 +81,227 @@ function SignUpForm({}: Props) {
     }
   };
 
-  // Dynamic card width based on screen size
   const cardWidth = useBreakpointValue({
-    base: "90vw",
+    base: "100%",
     md: "50vw",
     lg: "30vw",
   });
 
-  // Prevent rendering until hydration is complete
   if (!isHydrated) {
     return null;
   }
 
   return (
     <Box
-      bg="gray.200"
+      bg="#f4f4f6"
       width="100vw"
       height="100vh"
-      py={{ base: 6, md: 10 }}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
       px={{ base: 4, md: 8 }}
     >
-      <Center>
-        <Box mt={{ base: 6, md: 10 }}>
+      <Card
+        bg="white"
+        variant="outline"
+        borderColor="#d8dee4"
+        w={cardWidth}
+        p={{ base: 4, md: 6 }}
+        borderRadius="xl"
+      >
+        <CardBody>
           <Box mb={{ base: 6, md: 8 }} textAlign="center">
+            <Image
+              src="/logo.jpeg"
+              alt="Logo"
+              width={50}
+              height={50}
+              style={{ margin: "0 auto" }}
+            />
             <Heading
               as="h1"
               fontWeight="500"
-              fontSize={{ base: "24px", md: "28px", lg: "32px" }}
+              fontSize={{ base: "15px", md: "12px", lg: "18px" }}
               letterSpacing="-0.5px"
+              mt={4}
             >
               Sign up for Eatery
             </Heading>
           </Box>
 
-          {/* Sign Up Form */}
-          <Card
-            bg="#f6f8fa"
-            variant="outline"
-            borderColor="#d8dee4"
-            w={cardWidth}
-            p={{ base: 4, md: 6 }}
-            borderRadius="xl"
-          >
-            <CardBody>
-              <form onSubmit={handleSubmit}>
-                <Stack
-                  spacing={{ base: 4, md: 6 }}
-                  direction={{ base: "column", md: "row" }}
-                  mb="4"
-                >
-                  {/* First Name */}
-                  <FormControl isRequired>
-                    <FormLabel size="sm">First Name:</FormLabel>
-                    <Input
-                      type="text"
-                      value={firstName}
-                      onChange={(e) => setFirstName(e.target.value)}
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    />
-                  </FormControl>
-
-                  {/* Last Name */}
-                  <FormControl isRequired>
-                    <FormLabel size="sm">Last Name:</FormLabel>
-                    <Input
-                      type="text"
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    />
-                  </FormControl>
-                </Stack>
-
-                <Stack
-                  spacing={{ base: 4, md: 6 }}
-                  direction={{ base: "column", md: "row" }}
-                  mb="4"
-                >
-                  {/* Organization Name */}
-                  <FormControl isRequired>
-                    <FormLabel size="sm">Organization Name:</FormLabel>
-                    <Input
-                      type="text"
-                      value={organizationName}
-                      onChange={(e) => setOrganization(e.target.value)}
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    />
-                  </FormControl>
-
-                  {/* User Type */}
-                  <FormControl isRequired>
-                    <FormLabel size="sm">Category</FormLabel>
-                    <Select
-                      value={userType}
-                      onChange={(e) => setUserType(e.target.value)}
-                      placeholder="Select category"
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    >
-                      <option value="Admin">Admin</option>
-                      <option value="POSManager">POS Manager</option>
-                      <option value="MenuManager">Menu Manager</option>
-                      <option value="InventoryManager">
-                        Inventory Manager
-                      </option>
-                    </Select>
-                  </FormControl>
-                </Stack>
-
-                <Stack
-                  spacing={{ base: 4, md: 6 }}
-                  direction={{ base: "column", md: "row" }}
-                  mb="4"
-                >
-                  {/* Email Address */}
-                  <FormControl isRequired>
-                    <FormLabel size="sm">Email address:</FormLabel>
-                    <Input
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    />
-                  </FormControl>
-
-                  {/* Phone Number */}
-                  <FormControl isRequired>
-                    <FormLabel size="sm">Phone no:</FormLabel>
-                    <Input
-                      type="tel"
-                      value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    />
-                  </FormControl>
-                </Stack>
-
-                {/* Password */}
-                <Box mb="4">
-                  <FormControl isRequired>
-                    <FormLabel size="sm">Password</FormLabel>
-                    <Input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      bg="white"
-                      borderColor="#d8dee4"
-                      size="md"
-                      borderRadius="6px"
-                    />
-                  </FormControl>
-                </Box>
-
-                {/* Submit Button */}
-                <Button
-                  type="submit"
-                  bg="#2da44e"
-                  color="white"
+          <form onSubmit={handleSubmit}>
+            <Stack
+              spacing={{ base: 4, md: 6 }}
+              direction={{ base: "column", md: "row" }}
+              mb="4"
+            >
+              {/* First Name */}
+              <FormControl isRequired>
+                <FormLabel size="sm">First Name:</FormLabel>
+                <Input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
                   size="md"
-                  w="full"
-                  _hover={{ bg: "#2c974b" }}
-                  _active={{ bg: "#298e46" }}
+                  borderRadius="6px"
+                />
+              </FormControl>
+
+              {/* Last Name */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Last Name:</FormLabel>
+                <Input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
+                />
+              </FormControl>
+            </Stack>
+
+            <Stack
+              spacing={{ base: 4, md: 6 }}
+              direction={{ base: "column", md: "row" }}
+              mb="4"
+            >
+              {/* Organization Name */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Organization Name:</FormLabel>
+                <Input
+                  type="text"
+                  value={organizationName}
+                  onChange={(e) => setOrganization(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
+                />
+              </FormControl>
+
+              {/* User Type */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Category</FormLabel>
+                <Select
+                  value={userType}
+                  onChange={(e) => setUserType(e.target.value)}
+                  placeholder="Select category"
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
                 >
-                  Sign up
-                </Button>
-                <Center mt="2">
-                  <HStack fontSize="sm" spacing="1">
-                    <Text>Already have an account?</Text>
-                    <Link isExternal color="#0969da" href="/login">
-                      Sign in.
-                    </Link>
-                  </HStack>
-                </Center>
-                {error && (
-                  <Text color="red.500" fontSize="sm" mt={2}>
-                    {error}
-                  </Text>
-                )}
-              </form>
-            </CardBody>
-          </Card>
-        </Box>
-      </Center>
+                  <option value="Admin">Admin</option>
+                  <option value="POSManager">POS Manager</option>
+                  <option value="MenuManager">Menu Manager</option>
+                  <option value="InventoryManager">Inventory Manager</option>
+                </Select>
+              </FormControl>
+            </Stack>
+
+            <Stack
+              spacing={{ base: 4, md: 6 }}
+              direction={{ base: "column", md: "row" }}
+              mb="4"
+            >
+              {/* Email Address */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Email address:</FormLabel>
+                <Input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
+                />
+              </FormControl>
+
+              {/* Phone Number */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Phone no:</FormLabel>
+                <Input
+                  type="tel"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
+                />
+              </FormControl>
+            </Stack>
+
+            <Stack
+              spacing={{ base: 4, md: 6 }}
+              direction={{ base: "column", md: "row" }}
+              mb="4"
+            >
+              {/* Password */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Password</FormLabel>
+                <Input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
+                />
+              </FormControl>
+
+              {/* Confirm Password */}
+              <FormControl isRequired>
+                <FormLabel size="sm">Confirm Password</FormLabel>
+                <Input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  bg="white"
+                  borderColor="#d8dee4"
+                  size="md"
+                  borderRadius="6px"
+                />
+              </FormControl>
+            </Stack>
+
+            {/* Submit Button */}
+            <Button
+              type="submit"
+              bg="#2da44e"
+              color="white"
+              size="md"
+              w="full"
+              _hover={{ bg: "#2c974b" }}
+              _active={{ bg: "#298e46" }}
+              mt="4"
+            >
+              Sign up
+            </Button>
+
+            <Center mt="2">
+              <HStack fontSize="sm" spacing="1">
+                <Text>Already have an account?</Text>
+                <Link color="#0969da" href="/login">
+                  Login.
+                </Link>
+              </HStack>
+            </Center>
+
+            {error && (
+              <Text color="red.500" fontSize="sm" mt={2}>
+                {error}
+              </Text>
+            )}
+          </form>
+        </CardBody>
+      </Card>
     </Box>
   );
 }
